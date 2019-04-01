ff2d933b079f6e48f652abab4e5557fdb9516b7f
Krzysztof Dabrowski <01133318@pw.edu.pl>
2019-04-01 21:30:08 +0200

    Dodanie etatów do pracownika

--- Kod/src/main/java/pw/pio/Pracownik.java
+++ Kod/src/main/java/pw/pio/Pracownik.java
@@ -6,10 +6,29 @@ import java.util.Set;
 
 public class Pracownik extends Osoba {
     private int id;
+    private Set<Etat> etaty = new HashSet<>();
 
     public Pracownik(String imie, String nazwisko, int id) {
         super(imie, nazwisko);
         this.id = id;
+    }
+
+    public double policzWynagrodzenie()
+    {
+        double wynagrodzenie = 0;
+        for (Etat etat : etaty) {
+            wynagrodzenie += etat.getWynagrodzenie();
+        }
+
+        return wynagrodzenie;
+    }
+
+    /**
+     * <strong>Metoda, której nie ma w diagramie</strong>
+     * @param etat etat do dodanai pracownikowi
+     */
+    public void dodajEtat(Etat etat) {
+        etaty.add(etat);
     }
 
     public int getId() {
@@ -19,5 +38,12 @@ public class Pracownik extends Osoba {
     public void setId(int id) {
         this.id = id;
     }
-    
+
+    public Set<Etat> getEtaty() {
+        return etaty;
+    }
+
+    public void setEtaty(Set<Etat> etaty) {
+        this.etaty = etaty;
+    }
 }
