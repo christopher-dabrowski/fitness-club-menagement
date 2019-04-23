66d7c191dbd840714a9ec0bca3c586469c2525b7
SiwyKrzysiek <k.fajny@gmail.com>
2019-04-23 13:21:52 +0200

    Pola nazwy i czasu trwania pakietu. Większe podłączenie do kontrolera

--- Kod/src/main/java/pw/pio/CreateClientController.java
+++ Kod/src/main/java/pw/pio/CreateClientController.java
@@ -1,5 +1,46 @@
 package pw.pio;
 
-public class CreateClientController {
+import javafx.event.Event;
+import javafx.fxml.FXML;
+import javafx.scene.control.*;
+import lombok.val;
 
+import java.time.LocalDate;
+
+public class CreateClientController {
+//    @FXML
+//    private javafx.scene.control.Label nameField;
+
+    @FXML
+    private TextField nameLabel;
+    @FXML
+    private TextField surnameLabel;
+    @FXML
+    private DatePicker passStartDatePicker;
+    @FXML
+    private Spinner passDurationSpinner;
+
+    @FXML
+    public void initialize() {
+        System.out.println("Tworzenie okna");
+
+        passStartDatePicker.setValue(LocalDate.now());
+
+        val valueFactory = new SpinnerValueFactory.IntegerSpinnerValueFactory(10, 90, 30);
+        passDurationSpinner.setValueFactory(valueFactory);
+    }
+
+//    @FXML
+//    private Spinner karnetDurationSpinner;
+
+//    {
+////        nameField.setText("Test bloku inicjalizacyjnego");
+////        karnetDurationSpinner.setValueFactory(new SpinnerValueFactory.IntegerSpinnerValueFactory(10, 90, 30));
+//    }
+
+    @FXML
+    public void submitButtonPressed(Event event)
+    {
+        System.out.println("Klient zatwierdzony");
+    }
 }
