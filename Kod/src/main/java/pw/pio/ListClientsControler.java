e36074e8f5a213567d4915f7afe01b4cdd7cf272
SiwyKrzysiek <k.fajny@gmail.com>
2019-04-23 22:48:27 +0200

    Powrót do głównego menu z ekranu listy klientów

--- Kod/src/main/java/pw/pio/ListClientsControler.java
+++ Kod/src/main/java/pw/pio/ListClientsControler.java
@@ -1,11 +1,18 @@
 package pw.pio;
 
 import javafx.collections.FXCollections;
+import javafx.event.Event;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.geometry.Orientation;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.ListView;
+import javafx.stage.Stage;
 import lombok.*;
 
+import java.io.IOException;
 import java.util.Date;
 import java.util.List;
 import java.util.stream.Collectors;
@@ -24,5 +31,22 @@ public class ListClientsControler {
         val list = FXCollections.observableArrayList(toDisplay);
         clientsListView.setItems(list);
         clientsListView.setOrientation(Orientation.VERTICAL);
+    }
+
+    @FXML
+    public void returnToMainMenu(Event event) {
+        try {
+            val root = (Parent) FXMLLoader.load(getClass().getResource("/App.fxml"));
+
+            val stage = new Stage();
+            stage.setTitle("Główne menu");
+            stage.setScene(new Scene(root));
+            stage.show();
+
+            ((Node)(event.getSource())).getScene().getWindow().hide();
+        } catch (IOException e)
+        {
+            System.exit(1);
+        }
     }
 }
