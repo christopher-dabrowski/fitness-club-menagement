135aa5e2560505ede927c80f11bd8489d9328372
SiwyKrzysiek <k.fajny@gmail.com>
2019-04-23 21:38:17 +0200

    Tworzenie klienta na podstawie danych

--- Kod/src/main/java/pw/pio/CreateClientController.java
+++ Kod/src/main/java/pw/pio/CreateClientController.java
@@ -8,6 +8,9 @@ import lombok.val;
 import lombok.var;
 
 import java.time.LocalDate;
+import java.time.ZoneId;
+import java.util.ArrayList;
+import java.util.Date;
 
 public class CreateClientController {
 //    @FXML
@@ -18,9 +21,9 @@ public class CreateClientController {
     @FXML
     private TextField surnameTextField;
     @FXML
-    private TextField passNameTextField;
-    @FXML
     private DatePicker passStartDatePicker;
+    @FXML
+    private TextField passNameTextField;
     @FXML
     private Spinner passDurationSpinner;
     @FXML
@@ -50,9 +53,69 @@ public class CreateClientController {
 ////        karnetDurationSpinner.setValueFactory(new SpinnerValueFactory.IntegerSpinnerValueFactory(10, 90, 30));
 //    }
 
+    /**
+     * Check if fields have correct values
+     *
+     * @param errorOut Localized error message describing firs invalid value.
+     * @return <c>True</c> if fields have correct values <c>False</c> otherwise.
+     */
+    private boolean isInputValid(ArrayList<String> errorsOut) {
+        boolean isOk = true;
+
+        if (!nameTextField.getText().matches("\\p{L}+")) {
+            errorsOut.add("Nieprawidłowe imie");
+            isOk = false;
+        }
+        if (!surnameTextField.getText().matches("\\p{L}+")) {
+            errorsOut.add("Nieprawidłowe nazwisko");
+            isOk = false;
+        }
+        //Pass start can be any date
+        if (!passNameTextField.getText().matches(".+")) {
+            errorsOut.add("Nieprawidłowa nazwa pakietu");
+            isOk = false;
+        }
+        //Pass duration already has data validation
+        if (!monthlyCostTextField.getText().matches("\\d+([\\.,]\\d{1,2})?")) { //Can have either . or , as decimal separator
+            errorsOut.add("Nieprawidłowy miesięczny koszt");
+            isOk = false;
+        }
+        //freeActivitiesChoiceBox is always valid as it's value from list
+
+        return isOk;
+    }
+
     @FXML
-    public void submitButtonPressed(Event event)
-    {
-        System.out.println("Klient zatwierdzony");
+    public void submitButtonPressed(Event event) {
+        var errors = new ArrayList<String>(4);
+        if (!isInputValid(errors)) {
+            val alert = new Alert(Alert.AlertType.ERROR);
+            alert.setTitle("Nieprawidłowe dane");
+            alert.setHeaderText("Niektóre pola mają nieprawidłowe wartości");
+            alert.setContentText(String.join("\n", errors));
+
+            alert.showAndWait();
+
+            return;
+        }
+
+        val cost = Double.parseDouble(monthlyCostTextField.getText().replace(',', '.'));
+        val freeActivity = freeActivitiesChoiceBox.getValue().equals("True") ? true : false;
+
+        val newClient = new Klient
+                (
+                        nameTextField.getText(),
+                        surnameTextField.getText(),
+                        Date.from(passStartDatePicker.getValue().atStartOfDay(ZoneId.systemDefault()).toInstant()),
+                        new PakietUslug
+                                (
+                                        passNameTextField.getText(),
+                                        (int) passDurationSpinner.getValue(),
+                                        cost,
+                                        freeActivity
+                                )
+                );
+
+        System.out.println(newClient.toString());
     }
 }
