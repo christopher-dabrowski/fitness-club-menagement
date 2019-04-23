fd606c7b58b3e40c5e2e58f69b27cd5ed3ccfbed
SiwyKrzysiek <k.fajny@gmail.com>
2019-04-23 22:46:30 +0200

    Wyświetlanie listy klientów

--- Kod/src/main/java/pw/pio/ListClientsControler.java
+++ Kod/src/main/java/pw/pio/ListClientsControler.java
@@ -1,4 +1,28 @@
 package pw.pio;
 
+import javafx.collections.FXCollections;
+import javafx.fxml.FXML;
+import javafx.geometry.Orientation;
+import javafx.scene.control.ListView;
+import lombok.*;
+
+import java.util.Date;
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class ListClientsControler {
+    @FXML
+    private ListView clientsListView;
+
+    @FXML
+    public void initialize() {
+        val clients = BazaKlientow.getInstance().getKlienci();
+
+//        clients.add(new Klient("Jan", "Kowalski", new Date(), null));
+
+        List<String> toDisplay = clients.stream().map(c -> c.getImie() + " " + c.getNazwisko()).collect(Collectors.toList());
+        val list = FXCollections.observableArrayList(toDisplay);
+        clientsListView.setItems(list);
+        clientsListView.setOrientation(Orientation.VERTICAL);
+    }
 }
