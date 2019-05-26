442e248f3dc70007204795095efa8b6be1b1ed0f
Krzysztof Dabrowski <01133318@pw.edu.pl>
2019-05-26 19:01:55 +0200

    Test generacji id

---
+++ Kod/src/test/java/pw/pio/BazaRezerwacjiTest.java
@@ -0,0 +1,28 @@
+package pw.pio;
+
+import org.junit.Test;
+
+import java.util.Date;
+
+import static org.junit.Assert.*;
+
+public class BazaRezerwacjiTest {
+
+    @Test
+    public void poDodaniuNowejRezerwacjiUnikalnyNumerPowinienSieZmienic() {
+        //Given
+        final int staryNumer = BazaRezerwacji.getInstance().wygenerujUnikalnyNumerRezerwacji();
+
+        //When
+        Rezerwacja rezerwacja = new Rezerwacja(new Date(), 0, 0);
+        BazaRezerwacji.getInstance().dodajRezerwacje(rezerwacja);
+
+        //Then
+        final int nowyNumer = BazaRezerwacji.getInstance().wygenerujUnikalnyNumerRezerwacji();
+
+        assertNotEquals(staryNumer, nowyNumer);
+
+        //Cleanup
+        BazaRezerwacji.getInstance().listaRezerwacji().remove(rezerwacja);
+    }
+}
