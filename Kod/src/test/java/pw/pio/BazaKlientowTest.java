be3d139e90875f31a1c8ba2ac651ed1ec6f7ce36
KasiaWiktoria <48280406+KasiaWiktoria@users.noreply.github.com>
2019-05-28 18:25:58 +0200

    test DodajKlienta i UsunKlienta

---
+++ Kod/src/test/java/pw/pio/BazaKlientowTest.java
@@ -0,0 +1,42 @@
+package pw.pio;
+
+import org.junit.Test;
+
+import java.util.Date;
+
+import static org.junit.Assert.*;
+
+public class BazaKlientowTest {
+
+    @Test
+    public void poDodaniuKlientaKlientPowinienBycWBazie() {
+        //Given
+        BazaKlientow bazaKlientow = BazaKlientow.getInstance();
+
+        //When
+        Klient klient = new Klient("Mariusz", "Wysocki", new Date(), null);
+        bazaKlientow.dodajKlienta(klient);
+
+        //Then
+        assertTrue(bazaKlientow.listaKlientow().containsValue(klient));
+
+        //Cleanup
+        bazaKlientow.listaKlientow().remove(klient);
+    }
+
+    @Test
+    public void poUsunieciuKlientaKlientaNiePowinnoBycWBazie() {
+        //Given
+        BazaKlientow bazaKlientow = BazaKlientow.getInstance();
+        Klient klient = new Klient("Mariusz", "Wysocki", new Date(), null);
+        bazaKlientow.dodajKlienta(klient);
+
+        //When
+        if(bazaKlientow.listaKlientow().containsValue(klient)) {
+            bazaKlientow.usunKlienta(klient.getNumerKarnetu());
+            //Then
+            assertFalse(bazaKlientow.listaKlientow().containsValue(klient));
+        }else
+            fail("Wystąpił problem z dodaniem klienta do bazy");
+    }
+}
