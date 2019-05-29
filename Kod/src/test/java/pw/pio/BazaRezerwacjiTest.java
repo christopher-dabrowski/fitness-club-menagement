a59e94a950eea7686802548eaf3ac054042c3532
SiwyKrzysiek <k.fajny@gmail.com>
2019-05-29 12:24:52 +0200

    Testy bazy rezerwacji

--- Kod/src/test/java/pw/pio/BazaRezerwacjiTest.java
+++ Kod/src/test/java/pw/pio/BazaRezerwacjiTest.java
@@ -1,5 +1,6 @@
 package pw.pio;
 
+import lombok.val;
 import org.junit.Test;
 
 import java.util.Date;
@@ -24,5 +25,43 @@ public class BazaRezerwacjiTest {
 
         //Cleanup
         BazaRezerwacji.getInstance().listaRezerwacji().remove(rezerwacja.getIdRezerwacji());
+    }
+
+    @Test
+    public void getInstanceReturnsInstance() {
+        assertNotNull(BazaRezerwacji.getInstance());
+    }
+
+    @Test
+    public void getInstanceReturnsSameObject() {
+        //Given
+        val baza = BazaRezerwacji.getInstance();
+
+        //When
+        val druga = BazaRezerwacji.getInstance();
+
+        //Then
+        assertEquals(druga, baza);
+    }
+
+    @Test
+    public void poDodaniuRezerwacjiPowinnaBycWBazie() {
+        //Given
+        val rezerwacja = new Rezerwacja(new Date(), 0, 0);
+        val idRezerwacji = rezerwacja.getIdRezerwacji();
+        val bazaRezerwacji = BazaRezerwacji.getInstance();
+
+        //When
+        bazaRezerwacji.dodajRezerwacje(rezerwacja);
+
+        //Then
+        assertTrue(bazaRezerwacji.listaRezerwacji().containsValue(rezerwacja));
+
+        //Cleanup
+        bazaRezerwacji.listaRezerwacji().remove(idRezerwacji, rezerwacja);
+    }
+
+    @Test
+    public void usunRezerwacje() {
     }
 }
