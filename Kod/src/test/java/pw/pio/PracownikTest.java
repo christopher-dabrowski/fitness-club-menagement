8e43dd8dbbadf66446dacc0a3f8f2fd63469f828
JanKodowanie <48965206+JanKodowanie@users.noreply.github.com>
2019-05-29 13:38:50 +0200

    Dodanie testow etatow do PracownikTest
    usunEtat, dodajEtat

--- Kod/src/test/java/pw/pio/PracownikTest.java
+++ Kod/src/test/java/pw/pio/PracownikTest.java
@@ -13,6 +13,38 @@ import static org.junit.Assert.*;
 public class PracownikTest {
 
     @Test
+
+    public void dodajEtat(){
+
+        Pracownik p = new Pracownik("Pan", "Paweł");
+        Etat e1 = new Etat("Recepcjonista", 30, 3000, new Date(), new Date());
+        Etat e2 = new Etat("Trener", 10, 2000, new Date(), new Date());
+
+        p.dodajEtat(e1);
+        p.dodajEtat(e2);
+
+        assertTrue(p.listaEtatow().contains(e1));
+        assertTrue(p.listaEtatow().contains(e2));
+    }
+
+
+    @Test
+
+    public void usunEtat(){
+
+        Pracownik p = new Pracownik("Pan", "Paweł");
+        Etat e1 = new Etat("Recepcjonista", 30, 3000, new Date(), new Date());
+
+
+        p.dodajEtat(e1);
+        assertTrue(p.listaEtatow().contains(e1));
+        p.usunEtat(e1);
+
+        assertFalse(p.listaEtatow().contains(e1));
+    }
+
+    
+    @Test
     public void policzWynagrodzeniePowinnoBycRowneSumieWynagrodzenZEtatow() {
         //Given
         Pracownik p = new Pracownik("Adam", "Twaróg");
